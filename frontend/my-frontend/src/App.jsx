10088e21f975b86123a15b3d255f5771cd5fb10e
Luis Villa <vluis@unc.edu>
2024-06-27 22:45:50 -0400

    Add New Sneaker functionality

--- frontend/my-frontend/src/App.jsx
+++ frontend/my-frontend/src/App.jsx
@@ -1,14 +1,24 @@
 import { useEffect } from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { getSneakers } from './api/SneakerService'
+import { getSneakers, saveSneaker, udpatePhoto } from './api/SneakerService'
 import './App.css'
 import Header from './components/Header'
 import SneakerList from './components/SneakerList'
 
 function App() {
+  const modalRef = useRef()
+  const fileRef = useRef()
   const [data, setData]= useState({})
   const [currentPage, setCurrentPage]= useState(0)
+  const [file, setFile] = useState(undefined)
+  const [values, setValues] = useState({
+    name: '',
+    brand: '',
+    model: '',
+    colorway: ''
+  })
 
   const getAllSneakers = async (page = 0, size = 10) => {
     try{
@@ -21,7 +31,36 @@ function App() {
     }
   }
 
-  const toggleModal = (show) => { console.log("I was clicked") }
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value })
+    console.log(values)
+  }
+
+  const handleNewSneaker = async (event) => {
+    event.preventDefault()
+    try{
+      const {data} = await saveSneaker(values);
+      const formData = new FormData()
+      formData.append('file', file, file.name)
+      formData.append('id', data.id)
+      const { data: photoUrl } = await udpatePhoto(formData)
+      toggleModal(false)
+      console.log(photoUrl)
+      setFile(undefined)
+      fileRef.current.value = null
+      setValues({
+        name: '',
+        brand: '',
+        model: '',
+        colorway: ''
+      })
+      getAllSneakers()
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  const toggleModal = show => show ? modalRef.current.showModal() : modalRef.current.close();
 
   useEffect(() => {
     getAllSneakers();
@@ -38,6 +77,46 @@ function App() {
         </Routes>
       </div>
     </main>
+
+
+    {/* Modal */}
+    <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Sneaker</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form onSubmit={handleNewSneaker}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text" value={values.name} onChange={onChange} name='name' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Brand</span>
+                <input type="text" value={values.brand} onChange={onChange} name='brand' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Model</span>
+                <input type="text" value={values.model} onChange={onChange} name='model' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Colorway</span>
+                <input type="text" value={values.colorway} onChange={onChange} name='colorway' required />
+              </div>
+              <div className="file-input">
+                <span className="details">Profile Photo</span>
+                <input type="file" onChange={(event) => setFile(event.target.files[0])} ref={fileRef} name='photo' required />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel</button>
+              <button type='submit' className="btn">Save</button>
+            </div>
+          </form>
+        </div>
+      </dialog>
    
     </>
   )
